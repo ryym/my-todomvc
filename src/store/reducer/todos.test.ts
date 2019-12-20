a3cee249fbb94c2ef8caf3fa1954fe8f9fab14be
ryym <ryym.64@gmail.com>
2019-12-20 11:04:31 +0900

    test: Add test for reducing ToggleCompletedAll

--- src/store/reducer/todos.test.ts
+++ src/store/reducer/todos.test.ts
@@ -27,3 +27,33 @@ it('removes todo', () => {
   expect(got).toEqual(want);
   expect(got).not.toBe(init);
 });
+
+it('toggles completed of all todos', () => {
+  const random: Todos = [
+    { id: 1, title: 'a', completed: false },
+    { id: 2, title: 'b', completed: true },
+    { id: 3, title: 'c', completed: false },
+  ];
+
+  const allCompleted = [
+    { id: 1, title: 'a', completed: true },
+    { id: 2, title: 'b', completed: true },
+    { id: 3, title: 'c', completed: true },
+  ];
+
+  const allUncompleted = [
+    { id: 1, title: 'a', completed: false },
+    { id: 2, title: 'b', completed: false },
+    { id: 3, title: 'c', completed: false },
+  ];
+
+  const toggleAll = (todos: Todos, completed: boolean) => {
+    return reduceTodos(todos, $todos.ToggleCompletedAll(completed));
+  };
+
+  expect(toggleAll(random, true)).toEqual(allCompleted);
+  expect(toggleAll(allUncompleted, false)).toEqual(allUncompleted);
+
+  expect(toggleAll(random, false)).toEqual(allUncompleted);
+  expect(toggleAll(allCompleted, false)).toEqual(allUncompleted);
+});
